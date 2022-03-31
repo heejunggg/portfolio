e41994654f3c991d372458cbb6c7dcd060a8c200
you!!haenam!!! <102421262+heejunggg@users.noreply.github.com>
2022-03-31 16:43:14 +0900

    CSS:fix section container

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*$(function(){
+    var $menu = $('.navbar_menu li');
+    var $contents = $('section');
+  /*console.log($contents);
+
+   $menu.click(function(event){
+
+    var idx = $(this).index();
+    var tt = $contents.eq(idx).offset().top;
+  console.log(tt);
+    $('html,body').animate({scrollTop:tt});
+   });
+
+});*/
