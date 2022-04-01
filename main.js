fb08afedca260c0f3610823a6f5e7cb191c4dfbe
you!!haenam!!! <102421262+heejunggg@users.noreply.github.com>
2022-04-02 00:50:22 +0900

    JS:transparents

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ document.addEventListener('scroll', () => {
         return;
     }
 
-    scrollIntoView(link);
+    scrollIntoView(link); //함수를아래로 했으니까 이제 지정만 해주고..
     });
     
 
@@ -40,7 +40,7 @@ document.addEventListener('scroll', () => {
 // Button Contact me를 click시 contact section으로 가라.
     const homeContactBtn= document.querySelector('.home_contact');
     homeContactBtn.addEventListener('click', () => {
-    scrollIntoView('#contact');
+    scrollIntoView('#contact'); //함수를아래로 했으니까 이제 지정만 해주고..
     });
 
     //줄여서 함수로 지정해준다.scrollTo가 공통으로 들어갔네..
@@ -48,3 +48,21 @@ document.addEventListener('scroll', () => {
         const scrollTo = document.querySelector(selector);
         scrollTo.scrollIntoView({behavior:"smooth"});
     }
+
+    //스크롤 하면 home이 점점 투명해지는것.
+    //1.home을 불러와서 변수지정---2.스크롤되면 점점 fade. 스크롤양?
+    /*home의 화면높이가 800이라면 0--->800일때 처음인 0일때는 투명도가 1이여야하고
+    높이가 중간인 400일때는 투명도가 0.5여야 하고, 800일때는 투명도가 0이여야한다.*/
+
+    const home = document.querySelector('.home_container');
+    const homeheight = home.getBoundingClientRect().height;
+    document.addEventListener('scroll', () => {
+        //console.log(homeheight); 몇인지 함 보자규 751이네..
+        //console.log(1 - window.scrollY / homeheight);
+        home.style.opacity = 1 - window.scrollY / homeheight;
+
+    });
+
+    
+
+    
