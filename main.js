f13f56881c07952faaef873d10f16f4a45b17bfb
you!!haenam!!! <102421262+heejunggg@users.noreply.github.com>
2022-04-01 23:44:13 +0900

    JS:home_contact

--- main.js
+++ main.js
@@ -17,22 +17,34 @@ document.addEventListener('scroll', () => {
 
 // Handle scrolling when tapping on the navbar menu
 // navbar menu를 클릭했을때 해당 내용이 나온다.
+    const navbarMenu = document.querySelector('.navbar_menu');
+    navbarMenu.addEventListener('click',(event) =>{
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link==null){
+        return;
+    }
 
-const navbarMenu = document.querySelector('.navbar_menu');
-navbarMenu.addEventListener('click', (event) => {
-    // 클릭이 되면 우리가 등록한 함수가 호출되게 함.
+    scrollIntoView(link);
+    });
+    
+
+    // 클릭이 되면 우리가 등록한 함수가 호출되게 함.()=>{}
     // console.log(event.target); 내가클릭한 li 메뉴가 나온다.
    
     // dataset 은 index에 data- 넣었던것 다 세트를 말함.
    // console.log(event.target.dataset.link);
 
-//    undifne이라고 나올수있다.
-   const target = event.target;
-   const link = target.dataset.link;
-   if(link==null){
-        return;
-   }
-   console.log(event.target.dataset.link);
-   const scrollTo = document.querySelector(link);
-   scrollTo.scrollIntoView({behavior: "smooth"});
-});
+
+//    undifne이라고 나올수있다.if(link==null)
+// Button Contact me를 click시 contact section으로 가라.
+    const homeContactBtn= document.querySelector('.home_contact');
+    homeContactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact');
+    });
+
+    //줄여서 함수로 지정해준다.scrollTo가 공통으로 들어갔네..
+    function scrollIntoView(selector){
+        const scrollTo = document.querySelector(selector);
+        scrollTo.scrollIntoView({behavior:"smooth"});
+    }
