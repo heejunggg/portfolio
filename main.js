72b20ab1742eee3a2571ecae241f417f2fbf45d2
you!!haenam!!! <102421262+heejunggg@users.noreply.github.com>
2022-04-03 14:31:47 +0900

    JS:remove acitv nav,mywork

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
+    //console.log(window.scrollY);
     console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
@@ -36,7 +36,7 @@ document.addEventListener('scroll', () => {
    // console.log(event.target.dataset.link);
 
 
-//    undifne이라고 나올수있다.if(link==null)
+// undifne이라고 나올수있다.if(link==null)
 // Button Contact me를 click시 contact section으로 가라.
     const homeContactBtn= document.querySelector('.home_contact');
     homeContactBtn.addEventListener('click', () => {
@@ -52,19 +52,18 @@ document.addEventListener('scroll', () => {
     //스크롤 하면 home이 점점 투명해지는것 해보자!
     //1. home을 불러와서 변수지정---
     /*2. home의 화면높이가 800이라면 scrollY(화면맨윗)가 0일때는 투명도가 1(다보여지고) 
-    중간인 scrolllY가 400일때는 투명도가 0.5, scrollY가 800일때는 투명도가 0이되게해보자*/
-
+    중간인 scrolllY가 400일때는 투명도가 0.5, scrollY가 800일때는 투명도가 0이되게해보자
+    home의  height을 알아야겠지.스크롤을 했을때 실행이 되는거니 이벤트로지정해보자.이것도 변수로지정해보자.
+    */
     const home = document.querySelector('.home_container');
     const homeheight = home.getBoundingClientRect().height;
     document.addEventListener('scroll', () => {
-        //console.log(homeheight); 몇인지 함 보자규 751이네..
-        //console.log(1 - window.scrollY / homeheight); 0나누기 homeheight
-        home.style.opacity = 1 - window.scrollY / homeheight;
-
+       //console.log( 1 - window.scrollY /homeheight);
+       home.style.opacity = 1- window.scrollY / homeheight;
     });
 
-    // 
-    // Home의 반정도 왔을때 arrow보이게
+    // Home의 반정도 왔을때 arrowup의 visible이 보이게 하고 아닐때는 visible이 안보이게하라.
+
     const arrowUp = document.querySelector('.arrow-up');
    document.addEventListener('scroll', () => {
         if(window.scrollY > homeheight / 2 ){
@@ -78,4 +77,36 @@ document.addEventListener('scroll', () => {
     arrowUp.addEventListener('click', () => {
         scrollIntoView('#home');
     });
-    
+
+    // Projects
+    //각 data-filter가 해당하는 data-type에 가도록
+    const workBtnContainer = document.querySelector('.work_categories');
+    const projectContainer = document.querySelector('.work_project');
+    const projects = document.querySelectorAll('.project');
+    workBtnContainer.addEventListener('click', (e) => {
+        const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+        if(filter == null){ //filter가 null이면 아무것도 해주지않는다.
+            return; 
+        }
+        //console.log(filter);
+
+        //Remove selection from the previous item and select the new one
+        const active = document.querySelector('.catagory_btn.selected');
+        active.classList.remove('selected');
+        const target =
+        e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+        target.classList.add('selected');
+
+        projectContainer.classList.add('anim-out');
+        setTimeout(() => {  
+            projects.forEach((project) => {
+                console.log(project.dataset.type);
+                if(filter === '*' || filter === project.dataset.type){
+                    project.classList.remove('invisible'); //같을때는 선택한것만보여지고
+                } else {
+                    project.classList.add('invisible'); //다를때는 안보여야되는것까지 다 보여진다.
+                }
+            });
+            projectContainer.classList.remove('anim-out');
+        },300); 
+     });
